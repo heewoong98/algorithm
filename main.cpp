5b03dff538303d7e964921122d6bf3fe8dc3dff4
heewoong98 <c.heewoong98@gmail.com>
2021-02-13 14:22:53 +0000

    first

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ *  author: chw0501
+ *  reference: 
+ *  created: 21.01.2021 09:59:53(UTC)
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define MOD 1000000007 
+#define INF 987654321 
+
+
+
+int main(){
+	printf("main.cp");
+	printf("e");
+	printf("f");
+	return 0;
+}
